964d036e63fd665e3f50e8db33bd12055b42714c
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-07-10 16:25:37 -0700

    outlines basic form elements

--- src/components/Main/Electricity/index.js
+++ src/components/Main/Electricity/index.js
@@ -1,3 +1,23 @@
 export default () => (
-  <h3>This is the Electricity form.</h3>
+  <div>
+    <h3>This is the electricity form</h3>
+    <form onSubmit='undefined'>
+      <input type='hidden' value='electricity' />
+      <input type='hidden' value='mwh' />
+      <label htmlFor='electricity_value'>Electricity usage (in MWh):
+        <input name='electricity_value' type='number' />
+      </label>
+      <label htmlFor='country'>Country:
+        <select name='country'>
+          <option value='us'>United States</option>
+        </select>
+      </label>
+      <label htmlFor='state'>State:
+        <select name='state'>
+          <option value='or'>Oregon</option>
+        </select>
+      </label>
+      <button type='submit'>Estimate</button>
+    </form>
+  </div>
 )
--- src/components/Main/Flight/index.js
+++ src/components/Main/Flight/index.js
@@ -1,3 +1,22 @@
 export default () => (
-  <h3>This is the Flight form.</h3>
+  <div>
+    <h3>This is the flight form</h3>
+    <form onSubmit='undefined'>
+      <input type='hidden' value='flight' />
+      <label htmlFor='passengers'>Number of passengers:
+        <input name='passengers' type='number' />
+      </label>
+      <label htmlFor='departure-airport'>Departure airport:
+        <select name='departure-airport'>
+          <option value='pdx'>PDX - Portland International Airport</option>
+        </select>
+      </label>
+      <label htmlFor='destination-airport'>Destination airport:
+        <select name='destination-airport'>
+          <option value='sfo'>SFO - San Francisco International Airport</option>
+        </select>
+      </label>
+      <button type='submit'>Estimate</button>
+    </form>
+  </div>
 )
