7e3dc7ec761707dc0faef0bf30c070bcda61de97
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-07-11 21:56:38 -0700

    adds redux  and sets up firebase implementation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
+// import reportWebVitals from './reportWebVitals';
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
+import { createFirestoreInstance } from 'redux-firestore'
 import App from './components/App'
-// import reportWebVitals from './reportWebVitals';
+import firebase from './firebase'
+
+const store = createStore()
+
+const rrfProps = {
+  firebase,
+  config: {
+    userProfiles: `users`,
+  },
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>,
+  </Provider>,
   document.getElementById(`root`)
 )
 
