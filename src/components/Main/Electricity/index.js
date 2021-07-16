89ddcfe92408c2716a58e7971ae8f337d484b208
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-07-16 10:42:39 -0700

    adds form submit handler and input data

--- src/components/Main/Electricity/index.js
+++ src/components/Main/Electricity/index.js
@@ -1,23 +1,51 @@
-export default () => (
-  <div>
-    <h3>This is the electricity form</h3>
-    <form onSubmit='undefined'>
-      <input type='hidden' value='electricity' />
-      <input type='hidden' value='mwh' />
-      <label htmlFor='electricity_value'>Electricity usage (in MWh):
-        <input name='electricity_value' type='number' />
-      </label>
-      <label htmlFor='country'>Country:
-        <select name='country' placeholder='United States'>
-          <option value='us'>United States</option>
-        </select>
-      </label>
-      <label htmlFor='state'>State:
-        <select name='state' placeholder='Oregon'>
-          <option value='or'>Oregon</option>
-        </select>
-      </label>
-      <button type='submit'>Estimate</button>
-    </form>
-  </div>
-)
+import subregions from '../../../Data/Subregions'
+import countries from '../../../Data/Countries'
+
+const electricityEstimateForm = () => {
+  function handleElectricityInput(event) {
+    event.preventDefault()
+    return console.log({
+      type: event.target.type.value,
+      electricity_unit: event.target.electricity_unit.value,
+      electricity_value: event.target.electricity_value.value,
+      country: event.target.country.value,
+      state: event.target.state.value,
+    })
+  }
+
+  return (
+    <div>
+      <h3>This is the electricity form</h3>
+      <form onSubmit={handleElectricityInput}>
+        <input type='hidden' name='type' value='electricity' />
+        <input type='hidden' name='electricity_unit' value='mwh' />
+        <label htmlFor='electricity_value'>Electricity usage (in MWh):
+          <input name='electricity_value' type='number' step='0.01' min='0' />
+        </label>
+        <label htmlFor='country'>Country:
+          <select name='country'>
+            {countries.map(country => <option
+              value={country.code}
+            >
+              {country.name}
+            </option>)
+            }
+          </select>
+        </label>
+        <label htmlFor='state'>State:
+          <select name='state'>
+            {subregions.map(subregion => <option
+              value={subregion.code}
+            >
+              {subregion.name}
+            </option>)
+            }
+          </select>
+        </label>
+        <button type='submit'>Estimate</button>
+      </form>
+    </div>
+  )
+}
+
+export default electricityEstimateForm
