aa093d1e8b59e823151beb70fee1a99e5c492ee1
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-07-16 14:23:59 -0700

    toggles visible form state with hooks

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { useCallback, useState } from 'react'
 import Header from '../Header'
 import About from './About'
 import Nav from './Nav'
@@ -9,17 +11,41 @@ import Vehicle from './Vehicle'
 import FuelCombustion from './FuelCombustion'
 import Footer from '../Footer'
 
-export default () => (
-  <>
-    <Header />
-    <About />
-    <Nav />
-    <Estimate />
-    <Electricity />
-    <Flight />
-    <Shipping />
-    <Vehicle />
-    <FuelCombustion />
-    <Footer />
-  </>
-)
+export default () => {
+  const [formSelection, selectForm] = useState(null)
+
+  const formCallback = useCallback(formSelection => {
+    selectForm(formSelection)
+  }, [])
+
+  console.log(formSelection)
+
+  let visibleState = null
+
+  if (formSelection === 0) {
+    visibleState = <Electricity />
+  } else if (formSelection === 1) {
+    visibleState = <Flight />
+  } else if (formSelection === 2) {
+    visibleState = <Shipping />
+  } else if (formSelection === 3) {
+    visibleState = <Vehicle />
+  } else if (formSelection === 4) {
+    visibleState = <FuelCombustion />
+  }
+
+  return (
+    <>
+      <Header />
+      <About />
+      <Nav mainCallback={formCallback} />
+
+      <div>
+        {visibleState}
+      </div>
+
+      <Estimate />
+      <Footer />
+    </>
+  )
+}
