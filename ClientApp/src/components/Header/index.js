bf687071c9e250589918546a546622f3bf2ed0be
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-07-28 14:00:36 -0700

    adds header styles

--- ClientApp/src/components/Header/index.js
+++ ClientApp/src/components/Header/index.js
@@ -2,15 +2,45 @@ import { Link } from 'react-router-dom'
 
 export default () => (
   <>
-    <span>sqstr</span>
-    <span>
-      <Link exact to='/'>Home</Link>
+    <span style={{
+      margin: `0px 20px 0px 20px`,
+    }}
+    >
+      <Link
+        exact
+        to='/'
+        style={{ fontFamily: `'Megrim', cursive`,
+          fontSize: `3em`,
+          color: `#EDC9AF`,
+          textShadow: `1px 1px 10px #EDC9AF`,
+          textDecoration: `none` }}
+      >sqstr</Link>
     </span>
-    <span>
-      <Link to='/about'>About</Link>
+    <span style={{
+      margin: `20px 0px 10px 20px`,
+    }}
+    >
+      <Link
+        to='/estimate'
+        style={{ fontFamily: `'Playfair Display', serif`,
+          fontSize: `1em`,
+          color: `#EDC9AF`,
+          textShadow: `1px 1px 10px #EDC9AF`,
+          textDecoration: `none` }}
+      >Estimate</Link>
     </span>
-    <span>
-      <Link to='/estimate'>Estimate</Link>
+    <span style={{
+      margin: `0px 0px 10px 20px`,
+    }}
+    >
+      <Link
+        to='/about'
+        style={{ fontFamily: `'Playfair Display', serif`,
+          fontSize: `1em`,
+          color: `#EDC9AF`,
+          textShadow: `1px 1px 10px #EDC9AF`,
+          textDecoration: `none` }}
+      >About</Link>
     </span>
   </>
 
