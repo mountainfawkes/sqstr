dff130107c3bfe1044998e3297daf73437ec49af
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-07-28 16:21:06 -0700

    tweaks hero subhead

--- ClientApp/src/components/Splash/Elevator.js
+++ ClientApp/src/components/Splash/Elevator.js
@@ -3,8 +3,8 @@ export default () => (
     <h1 style={{ fontSize: `3.5em` }}>Pay back the future.</h1>
     <p style={{ fontSize: `1.25em`,
       fontWeight: `bold` }}
-    >Click Estimate to build investment advice
+    >Investment advice
       based on peer-reviewed science and internationally accredited
-      carbon sequestration standards.</p>
+      carbon sequestration standards</p>
   </div>
 )
