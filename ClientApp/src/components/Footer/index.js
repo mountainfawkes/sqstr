8577475559dbd7bb4231773c2d64f3542bd7a6a4
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-07-28 14:21:06 -0700

    adds footer content and styles

--- ClientApp/src/components/Footer/index.js
+++ ClientApp/src/components/Footer/index.js
@@ -1,3 +1,22 @@
 export default () => (
-  <h1>Footer content</h1>
+  <div>
+    <p>Copyright &copy; 2021 Jonathan Stull</p>
+    <span style={{ margin: `0 10px 0 0` }}><a
+      href='https://github.com/mountainfawkes'
+      style={{ color: `#EDC9AF`,
+        textShadow: `1px 1px 10px #EDC9AF`,
+        textDecoration: `none` }}
+    >
+      Github</a>
+    </span>
+    <span>
+      <a
+        href='https://www.linkedin.com/in/jonathanstull'
+        style={{ color: `#EDC9AF`,
+          textShadow: `1px 1px 10px #EDC9AF`,
+          textDecoration: `none` }}
+      >
+        LinkedIn</a>
+    </span>
+  </div>
 )
